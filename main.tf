f4619123f1b7b64fa6d146e9ae5b31f1bb931325
CGdev19 <126539481+CGdev19@users.noreply.github.com>
2023-08-20 23:15:53 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "my_vm" {
+  ami           = var.ami //Ubuntu AMI
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = var.name_tag,
+  }
+}
+
+resource "aws_instance" "my_vm2" {
+  ami           = var.ami //Ubuntu AMI
+  instance_type = var.instance_type
+ 
+  tags = {
+    Name = var.name_tag,
+  }
+}
